a36de8efb6b74df2790fe67a46633cf84b298557
Aman Kulshreshtha <aman21102000@gmail.com>
2020-11-05 05:55:25 +0530

    completed project

--- TextDetector.py
+++ TextDetector.py
@@ -1,9 +1,16 @@
 import cv2
 import pytesseract
 
+def write_to_file(text):
+    with open('extractedtext', 'a') as f:
+        f.write(text)
+
+        f.close()
+
+
 path_to_pytesseract_executable = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
-video_file_name = 'video.mp4'
-frame_rate = 200
+video_file_name = 'videofile.mp4'
+frame_rate = 100
 
 
 pytesseract.pytesseract.tesseract_cmd = path_to_pytesseract_executable
@@ -23,9 +30,9 @@ while True:
         img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
         text = pytesseract.image_to_string(img)
 
-        print(text)
+        write_to_file(text)
 
-        if cv2.waitKey(100) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
